f366722b4b3a13e4defbf9aea8e607ff1b8f85f3
ayaahmed <ayaahmeed963@gmail.com>
2024-02-04 21:01:41 +0300

    massage

---
+++ index.js
@@ -0,0 +1,16 @@
+var myName='aya'
+console.log(myName)
+console.log("type of myName", typeof myName);
+var myAge=25
+console.log(myAge)
+console.log("type of myAge", typeof myAge);
+  var isStudent=true 
+  console.log(isStudent)
+  console.log("type of isStudent", typeof isStudent);
+
+  
+  firstName = "aya";
+     lastName = "ahmed";
+     fullname=firstName+""+lastName;
+     console.log(fullname);
+  
